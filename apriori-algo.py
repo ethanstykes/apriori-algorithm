9eb309b1aba95ba19801bb0c2c9a3e29ff7ab7dd
Ethan Stykes <31500520+ethanstykes@users.noreply.github.com>
2018-12-13 22:51:52 +0530

    Add files via upload

--- apriori-algo.py
+++ apriori-algo.py
@@ -16,17 +16,17 @@ for i in range(len(tables[0])):
         poplist.append(products[i])
 #print(poplist)
 for i in range(len(poplist)):
-    print("deleting:",poplist[i])
-    tables[0].pop(poplist[i],None)
+        print("deleting:",poplist[i])
+        tables[0].pop(poplist[i],None)
 products=list(tables[0].keys())
 print(tables[0])
 #print(products)
 c=2
 def notin(new,tablelist):
-    for existing in tablelist:
+  for existing in tablelist:
     if set(new)==set(existing):
-        return False
-            return True
+      return False
+  return True
 scans=len(tables[0])-c+1
 for i in range(scans):
     tables.append(tables[c-2])
@@ -34,38 +34,38 @@ for i in range(scans):
     tablelistnew=[]
     print("\n")
     for j in range(len(tables[c-1])):
-        for k in range(0,len(products)):
-            #print(products[k],tablelist[j])
-            if products[k][0] not in tablelist[j] and notin(tablelist[j]+products[k],tablelist):
-                tablelistnew.append(tablelist[j]+products[k])
-                tablelist.append(tablelist[j]+products[k])
+      for k in range(0,len(products)):
+        #print(products[k],tablelist[j])
+        if products[k][0] not in tablelist[j] and notin(tablelist[j]+products[k],tablelist):      
+          tablelistnew.append(tablelist[j]+products[k])
+          tablelist.append(tablelist[j]+products[k])
     #print(tablelistnew)
     newtable={}
     for j in range(len(tablelistnew)):
-        frequency=0
-            for k in range(len(transactions)):
-                flag=0
-for l in range(len(tablelistnew[j])):
-    if(tablelistnew[j][l] not in transactions[k]):
-        flag=-1
+      frequency=0
+      for k in range(len(transactions)):
+        flag=0
+        for l in range(len(tablelistnew[j])):
+          if(tablelistnew[j][l] not in transactions[k]):
+            flag=-1
             break
         if flag==0:
-            frequency+=1
-                newtable[tablelistnew[j]]=frequency
-            tables[c-1]=newtable
-entries=list(tables[c-1].keys())
-poplist=[]
-for j in range(len(tables[c-1])):
-    if(tables[c-1][entries[j]]<supmin):
+          frequency+=1
+      newtable[tablelistnew[j]]=frequency
+    tables[c-1]=newtable
+    entries=list(tables[c-1].keys())
+    poplist=[]
+    for j in range(len(tables[c-1])):
+      if(tables[c-1][entries[j]]<supmin):
         #print(tables[0][products[i]])
         poplist.append(entries[j])
     #print(poplist)
     print(tables[c-1])
     for j in range(len(poplist)):
-        print("deleting:",poplist[j])
-        tables[c-1].pop(poplist[j],None)
-print(tables[c-1])
-c+=1
+      print("deleting:",poplist[j])
+      tables[c-1].pop(poplist[j],None)
+    print(tables[c-1])
+    c+=1
 print("\n")
 for table in tables:
-    print(table)
+  print(table)
